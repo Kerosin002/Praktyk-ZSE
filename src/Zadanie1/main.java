fadf67426da3a550e8fdc789389c7635d058f05b
Александр Ничипорчук <alexnychyporchuk@gmail.com>
2020-11-02 16:02:58 +0100

    zadanie 1

--- src/Zadanie1/main.java
+++ src/Zadanie1/main.java
@@ -1,10 +1,16 @@
 package Zadanie1;
+import java.util.Scanner;
 
 public class main {
     public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
         System.out.println("Informacja o uczniu");
         uczen uczen = new uczen();
-        uczen.set_all("Oleksandr","Nychyporchuk",18);
+        uczen.Imie=in.next();
+        uczen.Nazwisko=in.next();
+        uczen.wiek=in.nextInt();
+
+        uczen.set_all(uczen.Imie, uczen.Nazwisko, uczen.wiek);
         uczen.info(uczen.Imie, uczen.Nazwisko, uczen.wiek);
 
     }
