06e1df9002645446d26541562cfaa85846cc74ec
Александр Ничипорчук <alexnychyporchuk@gmail.com>
2020-11-09 13:38:23 +0100

    PTAK

---
+++ src/Ptak/Ptakk.java
@@ -0,0 +1,16 @@
+package Ptak;
+
+public class Ptakk {
+    String nazwa;
+    String kolor;
+    String rozmiar;
+    double srWaga;
+
+    public Ptakk(String nazwa, String kolor, String rozmiar, double srWaga) {
+        this.nazwa = nazwa;
+        this.kolor = kolor;
+        this.rozmiar = rozmiar;
+        this.srWaga = srWaga;
+    }
+
+}
---
+++ src/Ptak/ptakMain.java
@@ -0,0 +1,8 @@
+package Ptak;
+
+public class ptakMain {
+    public static void main(String[] args){
+        Ptakk ptak1= new Ptakk("Golab","szary", "drobny",0.35);
+        
+    }
+}
